1e50f94b6fa01d339d6c8bfde9ab88e93fa13d99
izban <izban@mail.ru>
2017-06-16 17:01:13 +0300

    new plot

--- code/makePlot3.sh
+++ code/makePlot3.sh
@@ -1,7 +1,7 @@
 f(x) = a*(x**1.86)
-fit f(x) '3SumUnaryTest' u 1:2 via a
+fit f(x) '3SumRandomTest' u 1:2 via a
 title_f(a) = sprintf('f(x) = %ex^{1.86}', a)
 set xlabel "n"
 set ylabel "time, seconds"
-plot "3SumUnaryTest" w l, f(x) t title_f(a)
+plot "3SumRandomTest" w linespoints, f(x) t title_f(a)
 pause mouse
